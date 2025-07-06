cabf860568b97de2932c79c3fb72de6eebfce0a6
Nataliaemucci <88558558+Nataliaemucci@users.noreply.github.com>
2025-07-06 19:15:08 -0300

    fix - modificacion mi progreso

--- app/src/main/java/com/rockandcode/cursos/domain/models/RangeMedal.kt
+++ app/src/main/java/com/rockandcode/cursos/domain/models/RangeMedal.kt
@@ -3,7 +3,9 @@ package com.rockandcode.cursos.domain.models
 data class RangeMedal(
     val id: Int,
     val name: String,
+    val number: Int,
     val description: String,
+    val benefits: List<String> = emptyList(),
     val icon: String,
     val threshold: Int,
 )
