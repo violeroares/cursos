13704d12dd2a4064253b29290aa7d51ac18cd355
Nataliaemucci <88558558+Nataliaemucci@users.noreply.github.com>
2025-07-07 23:28:44 -0300

    fix- style LoginScreen

--- app/src/main/java/com/rockandcode/cursos/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/rockandcode/cursos/ui/screens/LoginScreen.kt
@@ -6,41 +6,28 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -60,20 +47,20 @@ fun LoginScreen(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val isDarkTheme = isSystemInDarkTheme()
+    var isFocused by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         logoVisible = true
     }
 
-    Column(
+    Box(
         modifier =
             Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
                 .background(MaterialTheme.colorScheme.background),
-        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        // ZONA AZUL CON FONDO Y CORNER CURVO
+        // Fondo azul con contenido
         Column(
             modifier =
                 Modifier
@@ -81,32 +68,35 @@ fun LoginScreen(
                     .background(
                         color =
                             if (isDarkTheme) {
-                                MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                                MaterialTheme.colorScheme.background
                             } else {
                                 Color(0xFF2B1BBA)
                             },
-                        shape = RoundedCornerShape(bottomEnd = 96.dp),
                     ).padding(top = 48.dp, bottom = 64.dp, start = 48.dp, end = 48.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            // Logo animado
             AnimatedVisibility(
                 visible = logoVisible,
                 enter =
                     fadeIn(tween(DURATION_MS)) +
                         scaleIn(
                             initialScale = 0.8f,
-                            animationSpec =
-                                tween(
-                                    DURATION_MS,
-                                ),
+                            animationSpec = tween(DURATION_MS),
                         ),
             ) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Top,
                 ) {
-                    Spacer(modifier = Modifier.height(64.dp))
+                    Image(
+                        painter =
+                            painterResource(
+                                id = if (isDarkTheme) R.drawable.icons_login_top else R.drawable.icons_login_top,
+                            ),
+                        contentDescription = "logo top",
+                        modifier = Modifier.size(height = 90.dp, width = 300.dp),
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
                     Image(
                         painter =
                             painterResource(
@@ -116,54 +106,179 @@ fun LoginScreen(
                         modifier = Modifier.size(height = 90.dp, width = 300.dp),
                     )
                     Text(
-                        text = "Aprende. Crece. Destacate",
+                        text = "Aprendé. Crecé. Destacáte",
                         style = MaterialTheme.typography.titleLarge,
                         fontWeight = FontWeight.SemiBold,
                         color = Color(0xFFC8EE4A),
                         textAlign = TextAlign.Center,
                     )
+                    Spacer(modifier = Modifier.height(64.dp))
+                    Image(
+                        painter =
+                            painterResource(
+                                id = if (isDarkTheme) R.drawable.icons_login_bottom else R.drawable.icons_login_bottom,
+                            ),
+                        contentDescription = "logo bottom",
+                        modifier = Modifier.size(height = 90.dp, width = 300.dp),
+                    )
+                    Spacer(modifier = Modifier.height(30.dp))
                 }
             }
+        }
 
-            Spacer(modifier = Modifier.height(96.dp))
-
+        // Formulario blanco superpuesto
+        Column(
+            modifier =
+                Modifier
+                    .align(Alignment.BottomCenter) // 👈 Ancla la columna al fondo de pantalla
+                    .fillMaxWidth()
+                    .background(
+                        color =
+                            if (isDarkTheme) {
+                                MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                            } else {
+                                MaterialTheme.colorScheme.background
+                            },
+                        shape = RoundedCornerShape(topStart = 45.dp, topEnd = 45.dp),
+                    ).padding(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             Text(
                 text = "Inicia sesión para comenzar tu aprendizaje",
+                color = if (!isDarkTheme) Color(0xFF2B1BBA) else MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.White,
                 textAlign = TextAlign.Center,
                 fontWeight = FontWeight.SemiBold,
             )
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(25.dp))
+
             // Email
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                singleLine = true,
-                leadingIcon = {
-                    Icon(Icons.Default.Email, contentDescription = "Email Icon")
-                },
-                modifier = Modifier.fillMaxWidth(),
-            )
+            val enabled = true
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .shadow(8.dp, shape = RoundedCornerShape(50))
+                        .background(Color.White, shape = RoundedCornerShape(50))
+                        .border(
+                            width = 1.dp,
+                            color = if (isFocused) Color(0xFF7B2FC5) else Color.LightGray,
+                            shape = RoundedCornerShape(50),
+                        ).padding(horizontal = 14.dp, vertical = 10.dp),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier
+                                .size(30.dp) // tamaño del círculo
+                                .background(
+                                    color = Color(0xFFF2E7FE),
+                                    shape = CircleShape,
+                                ),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Email,
+                            contentDescription = "Email Icon",
+                            tint = Color(0xFF7B2FC5),
+                            modifier = Modifier.size(20.dp),
+                        )
+                    }
+                    Spacer(Modifier.width(8.dp))
+                    BasicTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        enabled = enabled,
+                        textStyle =
+                            MaterialTheme.typography.bodyLarge.copy(
+                                color = if (enabled) Color.Black else Color.Gray,
+                            ),
+                        singleLine = true,
+                        modifier =
+                            Modifier
+                                .weight(1f)
+                                .onFocusChanged { isFocused = it.isFocused },
+                        decorationBox = { innerTextField ->
+                            if (email.isEmpty()) {
+                                Text(
+                                    text = "Email",
+                                    style = TextStyle(color = Color.Gray),
+                                )
+                            }
+                            innerTextField()
+                        },
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             // Password
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Contraseña") },
-                singleLine = true,
-                visualTransformation = PasswordVisualTransformation(),
-                leadingIcon = {
-                    Icon(Icons.Default.Lock, contentDescription = "Password Icon")
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth(),
-            )
-            // Recuperar contraseña (fuera de zona azul)
-            Spacer(modifier = Modifier.height(16.dp))
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .shadow(8.dp, shape = RoundedCornerShape(50))
+                        .background(Color.White, shape = RoundedCornerShape(50))
+                        .border(
+                            width = 1.dp,
+                            color = if (isFocused) Color(0xFF7B2FC5) else Color.LightGray,
+                            shape = RoundedCornerShape(50),
+                        ).padding(horizontal = 14.dp, vertical = 10.dp),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier
+                                .size(30.dp) // tamaño del círculo
+                                .background(
+                                    color = Color(0xFFF2E7FE),
+                                    shape = CircleShape,
+                                ),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = "Password Icon",
+                            tint = Color(0xFF7B2FC5),
+                            modifier = Modifier.size(20.dp),
+                        )
+                    }
+                    Spacer(Modifier.width(8.dp))
+                    BasicTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        enabled = enabled,
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        textStyle =
+                            MaterialTheme.typography.bodyLarge.copy(
+                                color = if (enabled) Color.Black else Color.Gray,
+                            ),
+                        singleLine = true,
+                        modifier =
+                            Modifier
+                                .weight(1f)
+                                .onFocusChanged { isFocused = it.isFocused },
+                        decorationBox = { innerTextField ->
+                            if (password.isEmpty()) {
+                                Text(
+                                    text = "Contraseña",
+                                    style = TextStyle(color = Color.Gray),
+                                )
+                            }
+                            innerTextField()
+                        },
+                    )
+                }
+            }
+
+            // Recuperar contraseña
+            Spacer(modifier = Modifier.height(25.dp))
             Text(
                 text = "¿Olvidaste tu contraseña?",
                 modifier =
@@ -171,32 +286,26 @@ fun LoginScreen(
                         .align(Alignment.End)
                         .clickable { onForgotPasswordClick() },
                 style = MaterialTheme.typography.bodyMedium,
-                color = Color.White,
+                fontWeight = FontWeight.SemiBold,
+                color = if (!isDarkTheme) Color(0xFF2B1BBA) else MaterialTheme.colorScheme.onBackground,
             )
-        }
-        Spacer(modifier = Modifier.height(32.dp))
 
-        Column(
-            modifier =
-                Modifier.padding(horizontal = 48.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
             // Botón Login
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(25.dp))
             Button(
                 onClick = { onLoginClick() },
                 modifier =
                     Modifier
                         .fillMaxWidth()
                         .height(50.dp),
-                shape = RoundedCornerShape(8.dp),
+                shape = RoundedCornerShape(24.dp),
                 colors =
                     ButtonDefaults.buttonColors(
                         containerColor = if (!isDarkTheme) Color(0xFF7B2FC5) else MaterialTheme.colorScheme.primary,
                         contentColor = if (!isDarkTheme) Color.White else MaterialTheme.colorScheme.onPrimary,
                     ),
             ) {
-                Text("Iniciar sesión", fontWeight = FontWeight.SemiBold)
+                Text("Iniciar sesión", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.bodyLarge)
             }
 
             // Registro
@@ -206,11 +315,10 @@ fun LoginScreen(
                 Text(
                     text = "Registrate",
                     color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.SemiBold,
                     modifier = Modifier.clickable { onRegisterClick() },
                 )
             }
         }
-
-        Spacer(modifier = Modifier.height(24.dp))
     }
 }
